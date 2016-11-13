32322f02827f1339cf8d96fa37a4cd2f6785e251
sulphur <piotr.kedziora27@gmail.com>
2016-11-13 11:58:42 +0100

    update

--- zgen_config.zsh
+++ zgen_config.zsh
@@ -22,16 +22,17 @@ if ! zgen saved; then
     zgen oh-my-zsh plugins/ruby
     zgen oh-my-zsh plugins/battery
     zgen oh-my-zsh plugins/brew
-    zgen oh-my-zsh plugins/brew-cask
+    #zgen oh-my-zsh plugins/brew-cask
     zgen oh-my-zsh plugins/heroku
     zgen oh-my-zsh plugins/tmux
     zgen oh-my-zsh plugins/git-extras
     zgen oh-my-zsh plugins/github
     zgen oh-my-zsh plugins/docker
-    zgen oh-my-zsh plugins/nvm
+    #zgen oh-my-zsh plugins/nvm
     #zgen oh-my-zsh plugins/rvm
     #zgen oh-my-zsh plugins/spring
     zgen oh-my-zsh plugins/z
+    zgen oh-my-zsh plugins/zsh-navigation-tools
 
     zgen load zsh-users/zsh-syntax-highlighting
     zgen load rimraf/k
